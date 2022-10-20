580bea0af3d9b6a09619e39caa098db866485a5c
mathanramm <meetmemathanram@yahoo.com>
2022-10-20 21:24:21 +0530

    added new features

--- assets/JS/custom.js
+++ assets/JS/custom.js
@@ -1,28 +1,58 @@
-//  function keypress(btnpay) {
-//   const press = document.getElementById(btnpay);
-//   if (press.style.display === "none") {
-//     press.style.display = "block";
-//   } else {
-//     press.style.display = "none";
-//   }
-// }
+const btn = document.getElementById("register");
 
-const btn = document.getElementById("btnpayy");
-const list = document.getElementById("condisp");
+btn.addEventListener("click", getuserdata);
 
-btn.addEventListener("click", func);
+function getuserdata() {
+  document.getElementById("first_name_error").innerHTML = "";
+  document.getElementById("last_name_error").innerHTML = "";
+  document.getElementById("state_name_error").innerHTML = "";
+  document.getElementById("success_response").innerHTML = "";
 
-function func() {
-  if (list.style.display === "none") {
-    list.style.display = "block";
-    btn.disabled = true;
-    btn.innerHTML = "Payment Details";
-    btn.style.backgroundColor = "blue";
-    btn.style.color = "red";
-    setTimeout(() => {
-      list.style.display = "none";
-    }, 50000);
-  } else {
-    list.style.display = "none";
+  const first_name = document.getElementById("first_name").value;
+  const last_name = document.getElementById("last_name").value;
+  const state_name = document.getElementById("state").value;
+  const gender_value = document.querySelector(
+    'input[name="gender"]:checked'
+  ).value;
+  // alert(gender_value);
+  // const state_text = state_name.options[state_name.selectedIndex].text;
+  // alert(state_text);
+  if (first_name == "") {
+    document.getElementById("first_name_error").innerHTML +=
+      "first name missing";
+    document.getElementById("first_name_error").style.color = "red";
+    return false;
   }
+  if (last_name == "") {
+    document.getElementById("last_name_error").innerHTML += "last name missing";
+    document.getElementById("last_name_error").style.color = "red";
+    return false;
+  }
+  if (state_name == "") {
+    document.getElementById("state_name_error").innerHTML += "state missing";
+    document.getElementById("state_name_error").style.color = "red";
+    return false;
+  }
+  if (gender_value == "") {
+    alert("gender value missing");
+  }
+  // } else if (state_name == "tn") {
+  //   // alert("Tamil Nadu");
+  // } else if (state_name == "ka") {
+  //   // alert("Karnataka");
+  // } else if (state_name == "kl") {
+  //   alert("Kerala");
+  // } else {
+  //   // alert("select_state");
+  // }
+
+  const full_name = first_name + " " + last_name;
+  const response =
+    "Hello" + " " + full_name + " " + "you have successfully registered!";
+  document.getElementById("success_response").innerHTML += response;
+  setTimeout(() => {
+    location.reload();
+  }, 10000);
+  //   console.log("js working fine");
+  //   console.log(first_name);
 }
